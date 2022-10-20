0711a3ae5b7e9550dd128844f69420e296181ccb
allfiction100806210913 <lili10080621@gmail.com>
2022-10-20 11:18:37 +0900

    update:master

--- app/controllers/farrows_controller.rb
+++ app/controllers/farrows_controller.rb
@@ -1,2 +1,19 @@
 class FarrowsController < ApplicationController
+  before_action :farrow_shuffle, only: [:index]
+  
+  def index
+   @farrow = Library.order("created_at DESC")
+  end
+
+  private
+
+  def farrow_shuffle
+    farrow = Library.order("created_at DESC")
+
+    farrow_1 = farrow.last(rand(25..29))
+    farrow_2 = farrow - farrow_1
+    farrow_3 = farrow_1 + farrow_2
+    farrow_3_1 = riffle_3[0,27].reverse
+    farrow_3_2 = riffle_3[rand(20..27),rand(27..)].reverse
+    farrow_4 = riffle_3_1.zip(riffle_3_2).flatten.compact
 end
--- app/controllers/librarys_controller.rb
+++ app/controllers/librarys_controller.rb
@@ -1,15 +1,67 @@
-class HindusController < ApplicationController
-
+class LibrarysController < ApplicationController
+  before_action :library_set, except: [:index, :new, :create, :show]
+    before_action :library_rundam_all, only: [:index, :show]
+    before_action :library_rundam_five, only: [:index, :show]
     before_action :library_hindu_shuffle, only: [:index, :show]
   
    def index
     @librarys = Library.order("created_at DESC")
    end
   
+   def new
+    @library = Library.new
+   end
   
+   def create
+     @library = Library.new(library_params)
+     if @library.save
+       redirect_to root_path
+     else
+       render :new
+     end
+   end
+   
+   def show
+    @librarys = Library.order("created_at DESC")
+   end
   
-               private
-
+   def edit
+   end
+  
+   def update
+    if @library.update(library_params)
+      redirect_to library_path
+    else
+      render :edit
+    end
+   end
+  
+   def destroy
+    if
+     @library.destroy
+     redirect_to root_path
+    else
+     redirect_to root_path
+    end
+   end
+  
+   private
+              def library_params
+                params.require(:library).permit(:card_type, :image, :numbers)
+              end
+  
+              def library_set
+                @library = Library.find(params[:id])
+              end
+  
+              def library_rundam_all
+                @library_rundam_all = Library.order("RAND()")
+              end
+  
+              def library_rundam_five
+                @library_rundam_five = Library.order("RAND()").limit(5)
+              end
+  
               def library_hindu_shuffle
   
                 library_hindu_shuffle = Library.order("created_at DESC")
